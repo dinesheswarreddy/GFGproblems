8923ed012a9da81a6abd43f447ace089c0d269e1
dinesheswarreddy <dinesheswarreddy655@gmail.com>
2025-03-07 16:03:40 +0530

    Longest Palindromic Subsequence

---
+++ Day113.py
@@ -0,0 +1,26 @@
+#Longest Palindromic Subsequence
+class Solution:
+    def longestPalinSubseq(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        
+        for i in range(n):
+            dp[i][i] = 1
+        
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        
+        return dp[0][n - 1]
+
+# Example 1: Hardcoded string
+sol = Solution()
+print(sol.longestPalinSubseq("bbabcbcab"))  # Output: 7
+
+# Example 2: User input
+user_input = input("Enter a string: ")
+print(sol.longestPalinSubseq(user_input))
